65c8a780f9fec55008f86f3fb6a5667754842bd5
LordImmaculate <01836390+LordImmaculate@users.noreply.github.com>
2024-04-27 02:12:03 +0200

    Test

--- src/extension.ts
+++ src/extension.ts
@@ -17,5 +17,6 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(verify, upload, format);
 }
 
+//GIT TEST
 // This method is called when your extension is deactivated
 export function deactivate() { }
