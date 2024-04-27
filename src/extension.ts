483cb618e513cba0c5c17813d88200add713697f
LordImmaculate <01836390+LordImmaculate@users.noreply.github.com>
2024-04-27 02:07:14 +0200

    Bug Fix

--- src/extension.ts
+++ src/extension.ts
@@ -5,22 +5,17 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "touchbarcommands" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('touchbarcommands.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from touchbarcommands!');
+	const verify = vscode.commands.registerCommand('extension.verify', () => {
+		vscode.commands.executeCommand('arduino.verify');
 	});
-
-	context.subscriptions.push(disposable);
+	const upload = vscode.commands.registerCommand('extension.upload', () => {
+		vscode.commands.executeCommand('arduino.upload');
+	});
+	const format = vscode.commands.registerCommand('extension.format', () => {
+		vscode.commands.executeCommand('editor.action.formatDocument');
+	});
+	context.subscriptions.push(verify, upload, format);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
